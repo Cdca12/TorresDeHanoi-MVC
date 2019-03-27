e043fcbdc16e5f4c9f3806099b92bb95c3a050d4
Carlos Contreras <cdca12mx@gmail.com>
2019-03-26 17:56:07 -0700

    Añadido tema
    Mejoras visuales

--- src/torresdehanoi/mvc/Main.java
+++ src/torresdehanoi/mvc/Main.java
@@ -1,5 +1,9 @@
 package torresdehanoi.mvc;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.nimbus.NimbusLookAndFeel;
+
 /**
  *
  * @author Carlos Contreras
@@ -7,13 +11,22 @@ package torresdehanoi.mvc;
 public class Main {
 
     public static void main(String[] args) {
+        añadirTema();
         TorresDeHanoiVista vista = new TorresDeHanoiVista();
         TorresDeHanoiModelo modelo = new TorresDeHanoiModelo();
         TorresDeHanoiControlador controlador = new TorresDeHanoiControlador(vista, modelo);
-        
+
         vista.setControlador(controlador);
         vista.lanzarVista();
-//        controlador.iniciarJuego(3);
     }
-    
+
+    public static void añadirTema() {
+        try {
+            UIManager.setLookAndFeel(new NimbusLookAndFeel());
+        } catch (UnsupportedLookAndFeelException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 }
